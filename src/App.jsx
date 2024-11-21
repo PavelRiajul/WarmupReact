113c3c4392475e664b04f9befdc53a512264832d
Riajul Islam <riajulpislam@gmail.com>
2024-11-21 11:04:36 +0600

    detail-page-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./Components/Home"
+import Details from "./Components/Details"
+
 function App() {
   return (
     <div className="h-screen w-screen flex">
-    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/details/:id" element={<Details/>}/>
+    </Routes>
     </div>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,18 @@
-
-
+import { Link } from "react-router-dom"
+import Nav from "./Nav"
 function Home() {
   return (
-    <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto" >
-    <div className=" mr-3 mb-3 card p-3 border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col">
+    <>
+    <Nav/>
+      <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto" >
+    <Link to={"/details/1"} className=" mr-3 mb-3 card p-3 border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col">
       <div className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center" 
       style={{backgroundImage:"url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)"}}></div>
       <h1 className="hover:text-blue-300">Lorem ipsum dolor sit amet.</h1>
-    </div>
+    </Link>
 </div>
+    </>
+  
 
   )
 }
