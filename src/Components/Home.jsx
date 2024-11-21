1c4d5b456526eb201f128a977b7756c8aa2f4a5f
Riajul Islam <riajulpislam@gmail.com>
2024-11-21 18:19:23 +0600

    category-filter-done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,39 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import Nav from "./Nav"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ProductContext } from "../utils/Context"
 import Loading from "./Loading";
+import axios from "../utils/axios";
 function Home() {
-  const [products] =  useContext(ProductContext );
-  //console.log(products)
+  const [products] =  useContext(ProductContext);
+  const {search}= useLocation();
+  const category =decodeURIComponent(search.split("=")[1]);
+  
+const [filterProducts, setfilterProducts] = useState(null)
+
+const getProductsCategory=async () =>{
+  try{
+    const {data} = await axios.get(`/products/category/${category}`)
+    setfilterProducts(data)
+  }catch(e){
+    console.log(e)
+  }
+};
+
+
+useEffect(()=>{
+  if(!filterProducts) setfilterProducts(products);
+if(category != 'undefined') getProductsCategory();
+},[category,products])
+
+console.log(filterProducts)
+
   return ( products ?
     <>
     <Nav/>
       <div className="h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto" >
         
-       {products.map((p,i)=>(
+       {filterProducts&& filterProducts.map((p,i)=>(
         <Link key={i} to={`/details/${p.id}`} 
         className=" mr-3 mb-3 card p-3 border shadow rounded w-[18%] h-[30vh] flex justify-center items-center flex-col">
 
